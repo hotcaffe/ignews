e63164931e456258bebe0572d18ecafeae345c59
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-06-02 07:31:01 -0300

    SubscribeButton and Home

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return <button className={styles.button}>Subscribe now</button>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,61 @@
-export default function Home() {
-  return (
-    <div>
-    </div>
-  )
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  };
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+      <main className={styles.content}>
+        <section className={styles.announce}>
+          <p>👏 Hey, welcome</p>
+          <h1>
+            News about <br />
+            the <mark>React</mark> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <mark>for {product.amount} month</mark>
+          </p>
+          <SubscribeButton />
+        </section>
+        <div className={styles.avatar}>
+          <img
+            src="/images/avatar.svg"
+            alt="Image of a lady coding on her desk"
+          />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1L61vaJbVWXA0SXTevtw14r7");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 60 //24 hours
+  };
+};
