9d95113db78476e3cbdd47dc0103fce442b6a3e7
Raphael Fusco <raphaelfusco.rar@gmail.com>
2022-06-06 00:02:39 -0300

    stripe api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,32 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 export function SubscribeButton() {
-  return <button className={styles.button}>Subscribe now</button>;
+  const {data: session} = useSession()
+
+  async function handleSubscription(){
+    if(!session){
+      signIn('github')
+      return;
+    }
+    
+    try {
+      const response = await api.post('/subscribe');
+
+      const {sessionId} = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId});
+    } catch (err) {
+      alert(err.message)
+    }
+
+  }
+
+
+  return <button className={styles.button} onClick={() => handleSubscription()}>Subscribe now</button>;
 }
   
